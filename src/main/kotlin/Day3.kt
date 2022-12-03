56db76d7541572e40b83b4ec1048fe025305f92d
Pedro Flores <pedro_flores_16@hotmail.com>
2022-12-03 06:46:21 +0000

    Optimization to Day 3 of Advent of Code

--- src/main/kotlin/Day3.kt
+++ src/main/kotlin/Day3.kt
@@ -7,28 +7,22 @@ fun sumPrioritiesSharedItemTypesInRucksacks(input: String): Int =
         .map { line ->
             splitString(line)
                 .let { (firstCompartment, secondCompartment) ->
-                    val rucksack = Rucksack(firstCompartment, secondCompartment)
-                    return@map rucksack.getCommonItems()
-                        .sumOf { allPriorities.getOrDefault(it, 0) }
+                    return@map Rucksack(firstCompartment, secondCompartment).getCommonItems()
+                        .calculatePriorities()
                 }
         }.sum()
 
-fun sumPrioritiesSharedItemTypesByElfGroups(input: String): Int {
-    val u = input.split("\\r?\\n|\\r".toRegex())
+fun sumPrioritiesSharedItemTypesByElfGroups(input: String): Int =
+    input.split("\\r?\\n|\\r".toRegex())
         .chunked(3)
         .map { elvesGroup ->
-            val first = elvesGroup.first()
-            val commonChars = first.fold(mutableSetOf<Char>()) { acc, item ->
+            return@map elvesGroup.first().fold<MutableSet<Char>>(mutableSetOf()) { acc, item ->
                 if (elvesGroup.all { it.contains(item) }) {
                     acc.add(item)
                 }
                 acc
-            }
-            return@map commonChars.sumOf { allPriorities.getOrDefault(it, 0) }
-        }
-
-    return u.sum()
-}
+            }.calculatePriorities()
+        }.sum()
 
 data class Rucksack(val firstCompartment: String, val secondCompartment: String) {
     fun getCommonItems() = firstCompartment.fold(mutableSetOf<Char>()) { acc, c ->
@@ -39,10 +33,12 @@ data class Rucksack(val firstCompartment: String, val secondCompartment: String)
     }.toSet()
 }
 
-private fun splitString(input: String) =
-    (input.length / 2).let {
-        arrayOf(input.substring(0, it), input.substring(it))
-    }
+private fun Set<Char>.calculatePriorities() =
+    this.sumOf { allPriorities.getOrDefault(it, 0) }
+
+private fun splitString(input: String) = (input.length / 2).let {
+    arrayOf(input.substring(0, it), input.substring(it))
+}
 
 private val lowercaseCharPriorities = ('a'..'z')
     .fold(mutableMapOf<Char, Int>()) { acc, c ->
