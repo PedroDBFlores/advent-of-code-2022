fa481b8715f54613b29b90e5e11914d23996c313
Pedro Flores <pedro_flores_16@hotmail.com>
2022-12-04 18:24:36 +0000

    Print all results by running main.

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,7 +1,7 @@
 import domain.Challenge
 import domain.Part
 
-val supportFiles = (1..3).associateWith { day ->
+val supportFiles = (1..4).associateWith { day ->
     getResourceFile("day${day}_input.txt")
 }
 
@@ -11,16 +11,35 @@ val challenges = arrayOf(
             Part(number = 1, input = supportFiles[1]!!, action = ::calorieCounting),
             Part(number = 2, input = supportFiles[1]!!, action = ::topThreeCalories),
         )
+    ),
+    Challenge(
+        day = 2, name = "Rock Paper Scissors", parts = listOf(
+            Part(number = 1, input = supportFiles[2]!!, action = ::calculateTotalScorePart1),
+            Part(number = 2, input = supportFiles[2]!!, action = ::calculateTotalScorePart2),
+        )
+    ),
+    Challenge(
+        day = 3, name = "Rucksack Optimization", parts = listOf(
+            Part(number = 1, input = supportFiles[3]!!, action = ::sumPrioritiesSharedItemTypesInRucksacks),
+            Part(number = 2, input = supportFiles[3]!!, action = ::sumPrioritiesSharedItemTypesByElfGroups),
+        )
+    ),
+    Challenge(
+        day = 4, name = "Camp Cleanup", parts = listOf(
+            Part(number = 1, input = supportFiles[4]!!, action = ::countElvesThatFullyOverlap),
+            Part(number = 2, input = supportFiles[4]!!, action = ::countElvesThatPartiallyOverlap),
+        )
     )
 )
 
 fun main(args: Array<String>) {
     println("Welcome to Advent of Code 2022:")
+    println("*".repeat(40))
     challenges.forEach { (day, name, parts) ->
         println("Challenge for Day $day - $name")
         parts.forEach { part ->
             println("$part result -> ${part.calculateResult()}")
         }
-        println("****************************************")
+        println("*".repeat(40))
     }
 }
